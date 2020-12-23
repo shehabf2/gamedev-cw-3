38ee05db68d478afc8b598b7a2c508752fe2e5eb
shehabf2 <fnshehab2@gmail.com>
2020-12-23 11:54:21 -0500

    Hacker Game on
    part 2 done

--- HackerGame/Assets/Script/HackerGame1.cs
+++ HackerGame/Assets/Script/HackerGame1.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class HackerGame1 : MonoBehaviour
 {
+
+    int level;
+    string currentScreen = "MainMenu";
+    string testPass;
+
   // Start is called before the first frame update
 void Start()
 {
@@ -18,4 +23,91 @@ void ShowMainMenu(string greeting) {
     Terminal.WriteLine("Press 2 for CPES");
     Terminal.WriteLine("Enter your selection: ");
 }
+
+    void SetLevel(string selectedLevel)
+    {
+        if (selectedLevel == "1")
+        {
+
+            level = 1;
+            Terminal.WriteLine("You selected level 1");
+            //SetRandomPass();
+
+        }
+        else if (selectedLevel == "2")
+        {
+
+            level = 2;
+            //SetRandomPass();
+            Terminal.WriteLine("You selected level 2");
+
+        }
+        else if (selectedLevel == "menu") {
+            // take back to main menu
+            ShowMainMenu("Welcome Back Hacker!");
+        
+        }
+        else
+        {
+
+            Terminal.WriteLine("No 5arabee6 please. Get yourself together.");
+
+        }
+    }
+
+
+    void OnUserInput(string input) {
+
+        if (currentScreen == "MainMenu") {
+            // set the level by userinput
+            SetLevel(input);
+
+        }
+        else if (currentScreen == "password") {
+            // checks user input
+           // CheckPassword(input);
+        }
+
+
+    }
+
+
+   /* void StartGame() {
+
+        Terminal.WriteLine("You have selected level: " + level + ". Play now!");
+        currentScreen = "password";
+    }
+
+    void SetRandomPass()
+    {
+
+        StartGame();
+
+        switch (level)
+        {
+
+            case 1:
+                testPass = "safe";
+                break;
+            case 2:
+                testPass = "safe";
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+
+    void CheckPassword(string userInput) {
+        if (userInput == testPass)
+        {
+            Terminal.WriteLine("you won!");
+        }
+        else {
+            Terminal.WriteLine("wrong input :(");
+        }
+    } */
 }
