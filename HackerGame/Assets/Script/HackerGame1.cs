7aa9056b06d0601d9109b4524dde0903a3851ada
shehabf2 <fnshehab2@gmail.com>
2020-12-23 10:51:40 -0500

    HackerGame
    Greeting main menu added

---
+++ HackerGame/Assets/Script/HackerGame1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackerGame1 : MonoBehaviour
+{
+  // Start is called before the first frame update
+void Start()
+{
+    ShowMainMenu("Hello Hacker");
+}
+
+//Main menu method
+void ShowMainMenu(string greeting) {
+    Terminal.WriteLine(greeting);
+    Terminal.WriteLine("What would you like to hack into?");
+    Terminal.WriteLine("Press 1 for coded");
+    Terminal.WriteLine("Press 2 for CPES");
+    Terminal.WriteLine("Enter your selection: ");
+}
+}
